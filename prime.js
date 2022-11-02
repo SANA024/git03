e48b4823c693af3553c9ab5cbe0e3355201dae54
aditya6773 <adityasharma18081999@gmail.com>
2022-11-02 16:49:32 +0530

    prime code were fixed

--- prime.js
+++ prime.js
@@ -1,10 +1,11 @@
+let a=6;
 let adi=0;
 for(let i=1;i<=a;i++){
-    if(a%i==2){
+    if(a%i==0){
         adi++
     }
 }
-if(momos==2){
+if(adi==2){
     console.log("True")
 }else{
     console.log("False")
